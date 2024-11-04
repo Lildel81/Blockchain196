79ffc93091bef36ea069c4404eeacee9e37d9c07
BenEklundCS <eklundbenjaminj@gmail.com>
2024-11-04 07:37:13 -0800

    removed db conn logic

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        console.log('Attempting to connect to MongoDB...');
-        console.log('URI:', process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        console.log("DB connection function called");
+        //console.log('Attempting to connect to MongoDB...');
+        //console.log('URI:', process.env.MONGO_URI);
+        //const conn = await mongoose.connect(process.env.MONGO_URI, {
             // Add connection options here if needed
-        });
+        //});
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
