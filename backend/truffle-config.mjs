ab9213e32d5bc8a55e4f9d25f07369abcb622956
Ben <eklundbenjaminj@gmail.com>
2024-11-19 10:32:05 -0800

    Fixed truffle and npm run dev, moving to test.js to simulate an election

--- backend/truffle-config.mjs
+++ backend/truffle-config.mjs
@@ -1,19 +1,23 @@
-// truffle-config.mjs
-import { resolve } from 'path';
-
 export default {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      gas: 800000,      
-      gasPrice: 2000000000     
-    },
+      host: "127.0.0.1", // Localhost
+      port: 8545,        // Standard Ethereum port
+      network_id: "*",   // Any network
+      gas: 800000,
+      gasPrice: 2000000000
+    }
   },
   compilers: {
     solc: {
-      version: "0.8.21" // Your Solidity version
+      version: "^0.8.21", // Use locally installed version
+      parser: "solcjs",  // Use the solc-js parser
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   }
 };
