7d84d3cdbed7d663ad954f7d3f3390fbc3916a7b
Terry Weatherman <terry.weatherman8112@gmail.com>
2024-11-24 12:23:43 -0800

    Added address functionality -- need to fix error when voting

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,37 +1,61 @@
-import React, {useState} from 'react';
+
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
-import { Box, Button, Heading, Container, Center, Stack, Image, Input, Text, Flex } from '@chakra-ui/react';
+import { Box, Button, Heading, Center, Text, Flex, Input } from '@chakra-ui/react';
 
-/* 
-::TODO::
-React component for our homepage
-Buttons need to implement calls to the backend API
-*/
-
-function Home () {
+function Home() {
     const [isSplit, setIsSplit] = useState(false);
+    const [address, setAddress] = useState("");
+    const [isAddressValid, setIsAddressValid] = useState(false);
+
+    const handleVote = async (vote) => {
+        try {
+            const response = await fetch('http://localhost/voting/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ address, vote }),
+            });
+            const result = await response.json();
+            alert(result.message || 'Vote submitted successfully!');
+        } catch (error) {
+            alert('Error submitting vote: ' + error.message);
+        }
+    };
+
+    const handleAddressChange = (e) => {
+        const enteredAddress = e.target.value;
+        setAddress(enteredAddress);
+        setIsAddressValid(enteredAddress.trim().length > 0); // Simple validation for non-empty address
+    };
 
     return (
-        <Box 
-            position = "relative"
+        <Box
+            position="relative"
             bgImage="url('https://www.downloadclipart.net/large/government-png-clipart.png'),
-                     url('https://www.downloadclipart.net/large/69168-european-stars-clipart.png')" 
-            bgSize="500px 400px, 340px 340px" // Makes sure the image covers the entire background
-            bgPosition="49.5% 840px, 50% 380px" 
+                     url('https://www.downloadclipart.net/large/69168-european-stars-clipart.png')"
+            bgSize="500px 400px, 340px 340px"
+            bgPosition="49.5% 840px, 50% 380px"
             bgRepeat="no-repeat, no-repeat"
-            minHeight="100vh" 
+            minHeight="100vh"
         >
-            
             <Navbar />
             <Heading textAlign={"center"} marginTop={"8vh"}>
-                <Text fontWeight="300" textColor="gray.600" fontSize="70"> 2024 DIGITAL BALLOT </Text>
+                <Text fontWeight="300" textColor="gray.600" fontSize="70">2024 DIGITAL BALLOT</Text>
             </Heading>
 
+            <Center marginTop="5vh">
+                <Input
+                    placeholder="Enter Ethereum Address"
+                    value={address}
+                    onChange={handleAddressChange}
+                    width="400px"
+                />
+            </Center>
+
             <Center height="60vh">
-                {/* If not split, show a single rounded button */}
                 {!isSplit ? (
                     <Button
-                        onClick={() => setIsSplit(true)} // Splits the button on click
+                        onClick={() => setIsSplit(true)}
                         size="4xl"
                         bg="gray.600"
                         color="white"
@@ -39,39 +63,30 @@ function Home () {
                         width="900px"
                         height="300px"
                         _hover={{ bg: "gray.700" }}
+                        isDisabled={!isAddressValid} // Disable if address is not valid
                     >
                         <Text fontWeight="400" fontSize="45" marginBottom={"1vh"}>
-                                    CLICK TO CAST VOTE
-                                </Text>
+                            CLICK TO CAST VOTE
+                        </Text>
                     </Button>
                 ) : (
-                    // If split, show two semi-circular buttons
                     <Flex width="900px" justify="space-between">
-                        {/* Left Semi-Circle Button */}
                         <Button
-                            onClick={
-                                () => {
-                                    setIsSplit(false)
-                                    console.log("Voting For Harris")
-                                // TODO: Pop up window. Ask for final confirmation
-                                // Display "BALLOT WAS SUCCESSFULLY CAST" Must login to new user to cast another.
-                                // Post a vote to API for Harris
-                                }
-                            } 
+                            onClick={() => handleVote('Blue')}
                             width="50%"
                             height="300px"
                             borderRadius="50px 0 0 50px"
                             bg="blue.700"
                             color="white"
                             _hover={{ bg: "blue.800" }}
+                            isDisabled={!isAddressValid}
                         >
                             <Box>
-                                
                                 <Text fontWeight="400" fontSize="30" color="gray.100" display="inline">
                                     KAMALA D. {" "}
                                 </Text>
-                                <Text fontWeight="500" fontSize="30" color="gray.100" display="inline" >
-                                     HARRIS
+                                <Text fontWeight="500" fontSize="30" color="gray.100" display="inline">
+                                    HARRIS
                                 </Text>
                                 <Text fontWeight="400" fontSize="20" color="gray.200" mt={2}>
                                     Tim Walz
@@ -79,30 +94,22 @@ function Home () {
                             </Box>
                         </Button>
 
-                        {/* Right Semi-Circle Button */}
                         <Button
-                            onClick={
-                                () => {
-                                    setIsSplit(false)
-                                    console.log("Voting For Harris")
-                                // TODO: Pop up window. Ask for final confirmation
-                                // Display "BALLOT WAS SUCCESSFULLY CAST" Must login to new user to cast another.
-                                // Post a vote to API for Trump
-                                }
-                            } 
+                            onClick={() => handleVote('Red')}
                             width="50%"
                             height="300px"
                             borderRadius="0 50px 50px 0"
                             bg="red.700"
                             color="white"
                             _hover={{ bg: "red.800" }}
+                            isDisabled={!isAddressValid}
                         >
                             <Box>
-                            <Text fontWeight="400" fontSize="30" color="gray.100" display="inline">
+                                <Text fontWeight="400" fontSize="30" color="gray.100" display="inline">
                                     DONALD J. {" "}
                                 </Text>
-                                <Text fontWeight="500" fontSize="30" color="gray.100" display="inline" >
-                                     TRUMP
+                                <Text fontWeight="500" fontSize="30" color="gray.100" display="inline">
+                                    TRUMP
                                 </Text>
                                 <Text fontWeight="400" fontSize="20" color="gray.200" mt={2}>
                                     JD Vance
