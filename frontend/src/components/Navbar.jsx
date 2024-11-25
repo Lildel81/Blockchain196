6210d2ff2e267d021f11ab373f2b3cfbe8233c55
baljotk <baljotkaur@csus.edu>
2024-11-24 21:49:11 -0800

    added information page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
 					base: "column",
 					sm: "row",
 				}}
-				bg={colorMode === "light" ? "gray.200" : "black.900"} 
+				//bg={colorMode === "light" ? "gray.200" : "black.900"} 
+				backgroundImage={'linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.8)), url("https://t3.ftcdn.net/jpg/00/22/35/86/360_F_22358642_TlOPo0uu0tAb22YINFwY1UDKxFON8TFR.jpg")'}
+				backgroundSize={"cover"}
 				p={5} 
 				width="100%"
 				
@@ -28,7 +30,7 @@ const Navbar = () => {
 					fontWeight={"600"}
 					textTransform={"uppercase"}
 					textAlign={"center"}
-					bgGradient={"linear(to-l, red.600, gray.500, blue.600)"}
+					bgGradient={"linear(to-l, red.500, blue.900, blue.400)"}
 					bgClip={"text"}
 				>
 					<Link to={"/"}>US Presidential Election</Link>
