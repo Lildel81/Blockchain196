e131adc358d88bc69026f81ef137d4223fc87634
ramprashant66 <ramprashant66@gmail.com>
2024-11-28 01:40:13 -0800

    Final push with Baljot's frontend designs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Info from "./pages/Info";
 import Results from "./pages/Results";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/Results' element={<Results />} />
+				<Route path='/info' element={<Info />} />
 			</Routes>
 		</Box>
 	);
